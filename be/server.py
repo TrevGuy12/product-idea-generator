660c1d6c214a88721e72a08e341243c39ef4fb66
Trevor Ortell <trevorortell2013@gmail.com>
2024-04-03 20:26:36 -0600

    firebase commit

--- be/server.py
+++ be/server.py
@@ -1,4 +1,17 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('./firebase.json')
+firebase_admin.initialize_app(cred)
+
+# Access Firestore database
+db = firestore.client()
+
+# Now you can use `db` to interact with your Firestore database
+
 
 app = Flask(__name__)
 
